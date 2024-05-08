38c6186463df12d8e154eca3458339b664de367c
Andrae Vang <andrae.vang@gmail.com>
2024-05-08 14:19:56 -0700

    added story

--- Variable Exercise/Variable Exercise/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-string name = "Eli";
-int age = 8;
-char middleInitial = 'C';
-bool isOver18 = false;
-double currentTemp = 80.9;
-decimal price = 109.50m;
-
-//Step 2: Interpolate variables in a console.writeline()
